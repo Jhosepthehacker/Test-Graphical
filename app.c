87a9146d3119b92daec55b60fa350e02360e9379
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-09-12 11:53:21 -0400

    Add files via upload

---
+++ app.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <GLES3/gl3.h>
+
+int main() {
+	const int WIDTH_WINDOW = 632;
+	const int HEIGHT_WINDOW = 626;
+	
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+	  printf("No se pudo inicializar SDL. Error: %s\n", SDL_GetError());
+	 
+	  return 1;
+  }
+  
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+  
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+
+SDL_Window* window = SDL_CreateWindow("My Game",
+  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+  WIDTH_WINDOW, HEIGHT_WINDOW,
+  SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+  
+  if (window == NULL) {
+    printf("La ventana falló. Error: %s\n", SDL_GetError());
+    SDL_Quit();
+  	
+    return 1;
+  }
+  
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+  
+  if (context == NULL) {
+    printf("No se pudo crear el contexto OpenGL. Error: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
+    
+    SDL_Quit();
+    
+    return 1;
+  }
+  
+  bool running = true;
+  SDL_Event event;
+  
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        running = false;
+      }
+    }
+    glClearColor(0.1f, 0.2f, 0.2f, 0.8f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    SDL_GL_SwapWindow(window);
+  }
+  
+  SDL_GL_DeleteContext(context);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+	return 0;
+}
